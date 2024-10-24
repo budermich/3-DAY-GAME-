6dd7114f3e47f2453427ebc7bc2bf84e7e2b3a28
budermich <budermich5@gmail.com>
2024-10-24 18:42:47 +0300

    asdas

---
+++ scenes/Obstacles/area_2d.gd
@@ -0,0 +1,18 @@
+extends Area2D
+var StartPos
+var dir = Vector2.LEFT
+var speed = 400
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	StartPos = position.x 
+	print(position)
+	move(delta)
+
+func move(delta):
+	position += dir*speed*delta
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
