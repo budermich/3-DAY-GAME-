7ed7438bba3eb94a93fcb0e439e42afe43017d8d
budermich <budermich5@gmail.com>
2024-10-28 20:26:41 +0200

    how

--- scripts/load_screen.gd
+++ scripts/load_screen.gd
@@ -1,5 +1,5 @@
 extends Node2D
 
 func _ready() -> void:
-	await get_tree().create_timer(1).timeout
+	await get_tree().create_timer(0.3).timeout
 	get_tree().change_scene_to_file("res://scenes/World Scenes/main.tscn")
